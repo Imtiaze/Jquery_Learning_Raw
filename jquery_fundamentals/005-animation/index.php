4dcf55a2abb04267bdd5f933524da8286f18a533
Imtiaze <ahammedimtiaze78@gmail.com>
2019-07-15 02:03:48 +0600

    jQuery Fundamental: Start animation stop animation

--- jquery_fundamentals/005-animation/index.php
+++ jquery_fundamentals/005-animation/index.php
@@ -1,7 +1,61 @@
 <?php include 'inc/header.php'; ?>
 
 
+<style type="text/css">
+	.para{
+		width: 825px;
+		height: 425px;
+		padding: 15px;
 
+		border: 1px solid #000;
+		background: #fff;
+		position: relative;
+		
+	}
+	.inside{
+		margin-top: 150px;
+		height: 100px;
+		width: 100px;
+		border: 5px solid #1111ff;
+		border-radius: 50%;
+		background-color: green;
+		position: absolute;
+	}
+</style>
+
+<button class="startAnimate">Start Animation</button>
+<button class="stopAnimate">Stop Animation</button>
+
+<div class="para">
+
+	<div class="inside">
+		
+	</div>
+	
+</div>
 
 
 <?php include 'inc/footer.php'; ?>
+
+<script type="text/javascript">
+	
+	$(document).ready(function() {
+
+		$(".startAnimate").click(function() {
+
+			$(".inside").animate({
+				left:'600px',	
+				opacity: '0.5',
+				height: '200px',
+				width: '200px'
+			}, 3000);
+
+		});
+
+		$(".stopAnimate").click(function(){
+			$(".inside").stop();
+		});
+
+	});
+
+</script>
